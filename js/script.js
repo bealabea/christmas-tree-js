fee0ae61a5cb5a97354b0102034db71139acb015
bealabea <beatricemarin89@yahoo.it>
2022-01-09 21:10:19 +0100

    add snow on the tree and setInterval to the lights (red-yellow)

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ for (let k = 0; k < 1; k++) {
   for (let j = 0; j < 11; j++) {
     let box = document.createElement("div");
     box.classList.add("box-style");
+    box.classList.add("border-box");
     outputHtml.append(box);
     if (j === starIndex) {
       // Dichiaro boxInside, creando un elemento
@@ -46,21 +47,67 @@ for (let k = 0; k < 1; k++) {
 
 let startIndex = 5;
 let endIndex = 5;
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 5; i++) {
   for (let j = 0; j < 11; j++) {
     let box = document.createElement("div");
     box.classList.add("box-style");
+    box.classList.add("border-box");
     outputHtml.append(box);
     if (j >= startIndex && j <= endIndex) {
       box.classList.add("colored");
+      box.classList.remove("border-box");
       let boxInside = document.createElement("div");
       boxInside.classList.add("balls-box");
       box.append(boxInside);
+      setInterval(function(){
+        boxInside.classList.toggle("yellow");
+      }, 1500);
     }
+    if(j === startIndex && j === endIndex){
+      box.classList.add("border-radius");
+    }
+    if (j === startIndex){
+      box.classList.add("border-start");
+    }
+    if(j === endIndex){
+      box.classList.add("border-end");
+    }  
   }
   startIndex--;
   endIndex++;
 }
+
+startIndex = 0;
+endIndex = 11;
+for(let g = 0; g < 1; g++) {
+  for (let j = 0; j < 11; j++) {
+    let box = document.createElement("div");
+    box.classList.add("box-style");
+    box.classList.add("border-box");
+    outputHtml.append(box);
+    if (j >= startIndex && j <= endIndex) {
+      box.classList.add("colored");
+      box.classList.remove("border-box");
+      box.classList.add("border-bot");
+      let boxInside = document.createElement("div");
+      boxInside.classList.add("balls-box");
+      box.append(boxInside);
+      setInterval(function(){
+        boxInside.classList.toggle("yellow");
+      }, 1500);
+    }
+    if(j === 5){
+      box.classList.remove("border-bot");
+    }
+    if (j === 0){
+      box.classList.add("border-left");
+    }
+    if(j === 10){
+      box.classList.add("border-right");
+    }  
+  }
+}
+
 
 startIndex = 5;
 endIndex = 5;
@@ -68,9 +115,11 @@ for (let k = 0; k < 2; k++) {
   for (let j = 0; j < 11; j++) {
     let box = document.createElement("div");
     box.classList.add("box-style");
+    box.classList.add("border-box");
     outputHtml.append(box);
     if (j === startIndex && j === endIndex) {
       box.classList.add("brown");
+      box.classList.remove("border-box");
     }
   }
 }
